e250bd33ced5e5397ddcbd33607f11c1727d9d48
S1lv3rn <crisada99@hotmail.com>
2018-11-04 13:51:19 +0000

    Adding EP2:  - adding arts   => use kira its a very good arts program  - adding diplay arts to code

--- ABitRacey.py
+++ ABitRacey.py
@@ -1,23 +1,39 @@
 import pygame
 
 pygame.init()
+display_width = 800
+display_height = 600
 
-gameDisplay = pygame.display.set_mode((800, 600))
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
+carImg = pygame.image.load('arts/car.png')
 
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
 crashed = False
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
         	crashed = True
-		
+
 		print(event)
-		
+
+    gameDisplay.fill(WHITE)
+    car(x,y)
 	pygame.display.update()
-	
+
 	clock.tick(50)
-	
+
 pygame.quit()
 quit()
